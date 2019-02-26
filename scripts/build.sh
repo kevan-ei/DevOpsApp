2348fd057b7095739d8f772be4036ebaf0460ab7
kevan <einfochips>
2019-02-26 12:38:02 +0530

    Build script added

---
+++ scripts/build.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+# get input params
+branchName=$1
+buildType=$2
+storePass=$3
+keyAlias=$4
+keyPass=$5
+
+# helper method
+setProperty() {
+	sed -i.bak -e "s/\($1 *= *\).*/\1$2/" ${propertiesFile}
+}
+
+propertiesFile='gradle.properties'
+chmod +x ${propertiesFile}
+
+# update key properties based on build type
+if [ $buildType = 'debug' ]; then
+	(setProperty "KEYSTORE" "debug.keystore")
+	(setProperty "STORE_PASSWORD" "123456")
+	(setProperty "KEY_ALIAS" "my_alias")
+	(setProperty "KEY_PASSWORD" "123456")
+elif [ $buildType = 'release' ]; then
+	(setProperty "KEYSTORE" "release.keystore")
+	(setProperty "STORE_PASSWORD" "$storePass")
+	(setProperty "KEY_ALIAS" "$keyAlias")
+	(setProperty "KEY_PASSWORD" "$keyPass")
+fi
+
+# clean project
+chmod +x gradlew
+./gradlew clean --stacktrace
+
+# build
+if [ $buildType = 'debug' ]; then
+	./gradlew assembleDebug --stacktrace
+elif [ $buildType = 'release' ]; then
+	./gradlew assembleRelease --stacktrace
+fi
+
+# ready expected final APK 
+apkFileName="app-$buildType.apk"
+
+# check if exists, if not exit with error
+if [ ! -e "app/build/outputs/apk/$buildType/$apkFileName" ]; then
+    echo "ERROR: File not exists: (app/build/outputs/apk/$buildType/$apkFileName)"
+    exit 1
+fi
+
+rm -r artifacts/
+mkdir artifacts
+cp app/build/outputs/apk/$buildType/$apkFileName artifacts/
+
+cat << "EOF"
+             ,
+         (`.  : \               __..----..__
+          `.`.| |:          _,-':::''' '  `:`-._
+            `.:\||       _,':::::'         `::::`-.
+              \\`|    _,':::::::'     `:.     `':::`.
+               ;` `-''  `::::::.                  `::\
+            ,-'      .::'  `:::::.         `::..    `:\
+          ,' /_) -.            `::.           `:.     |
+        ,'.:     `    `:.        `:.     .::.          \
+   __,-'   ___,..-''-.  `:.        `.   /::::.         |
+  |):'_,--'           `.    `::..       |::::::.      ::\
+   `-'                 |`--.:_::::|_____\::::::::.__  ::|
+                       |   _/|::::|      \::::::|::/\  :|
+                       /:./  |:::/        \__:::):/  \  :\
+                     ,'::'  /:::|        ,'::::/_/    `. ``-.__
+                    ''''   (//|/\      ,';':,-'         `-.__  `'--..__
+                                                             `''---::::'
+EOF
