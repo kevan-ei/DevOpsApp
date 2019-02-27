cd520998909579350b86174f739bffbcdc2ef39b
kevan <einfochips>
2019-02-27 15:26:35 +0530

    test for build trigger on push 4

--- app/src/main/java/com/example/devopsapp/MainActivity.java
+++ app/src/main/java/com/example/devopsapp/MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    //comment added to check build trigger with push 3
+    //comment added to check build trigger with push 4
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
