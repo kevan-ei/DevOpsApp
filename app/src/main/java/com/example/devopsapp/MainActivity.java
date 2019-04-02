b70c1d25ef3e88cf162bb00e89ad8bab7e8b49b0
kevan <einfochips>
2019-04-02 13:03:27 +0530

    no 6

--- app/src/main/java/com/example/devopsapp/MainActivity.java
+++ app/src/main/java/com/example/devopsapp/MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    //comment added to check build trigger with push 5
+    //comment added to check build trigger with push 6
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
